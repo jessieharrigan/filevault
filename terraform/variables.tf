c9805e3dbf2a0ff8a1f5f030fe3557dfa49c0543
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2025-12-18 14:03:40 +0000

    Add Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-filevault"
+}
+
+variable "location" {
+    default = "uksouth"
+}
+
+variable "azurerm_virtual_network" {
+    default = "vnet-filevault"
+}
