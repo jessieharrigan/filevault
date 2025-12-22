dff599eb923628e3f2d099d98de5943ebd85b518
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2025-12-22 22:25:46 +0000

    Update CD pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,6 @@
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.rg.name
 }
 
 output "storage_account_name" {
@@ -7,23 +8,22 @@ output "storage_account_name" {
   value       = azurerm_storage_account.storage.name
 }
 
-output "storage_account_key" {
-  description = "The primary access key for the storage account"
-  value       = azurerm_storage_account.storage.primary_access_key
-  sensitive   = true
-}
-
 output "container_name" {
   description = "The name of the storage container"
   value       = azurerm_storage_container.container.name
 }
 
-output "connection_string" {
-  description = "The connection string for the storage account"
-  value       = azurerm_storage_account.storage.primary_connection_string
-  sensitive   = true
+output "key_vault_name" {
+  description = "The name of the Key Vault"
+  value       = azurerm_key_vault.kv.name
 }
 
 output "app_url" {
-  value = "http://${azurerm_container_group.app.fqdn}:3000"
+  description = "The public FQDN of the application"
+  value       = "http://${azurerm_container_group.app.fqdn}:3000"
+}
+
+output "aci_principal_id" {
+  description = "The Principal ID of the ACI Managed Identity"
+  value       = azurerm_container_group.app.identity[0].principal_id
 }
