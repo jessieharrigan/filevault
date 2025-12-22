8b08aa82bca9672e7f15c45ba2a040a1dc0d6b6b
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2025-12-22 12:08:16 +0000

    Fix Dockerfile (#3)

--- src/azure-sa/index.js
+++ src/azure-sa/index.js
@@ -93,6 +93,10 @@ app.delete('/files/:key', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send('FileVault is Active 🚀');
 });
